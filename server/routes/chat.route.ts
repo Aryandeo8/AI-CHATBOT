5f39c6125213cffedaf3aab57416e136ac98cd3a
Aryandeo8 <aryandeo949@gmail.com>
2025-03-30 23:40:38 +0530

    THIS IS A ERROR COMMIT TO BE FIXED LATER

--- server/routes/chat.route.ts
+++ server/routes/chat.route.ts
@@ -1,2 +1,20 @@
 import { Router } from "express";
-import { chatCompletionValidator, validate } from "../utils/validators";
+import { chatCompletionValidator, validate } from "../utils/validators.ts";
+import { verifyJWT } from "../middleware/auth.middleware.ts";
+import {
+    deleteChats,
+    generateChatCompletion,
+    sendChatsToUser,
+} from "../controllers/chat.controller.ts";
+
+const chatRoutes= Router();
+chatRoutes.post(
+    "/new",
+    validate(chatCompletionValidator),
+    verifyJWT,
+    generateChatCompletion
+);
+chatRoutes.get("/all-chats", verifyJWT, sendChatsToUser);
+chatRoutes.delete("/delete", verifyJWT, deleteChats);
+
+export default chatRoutes;
